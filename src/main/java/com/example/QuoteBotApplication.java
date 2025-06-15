1e78ba8d3f3ba9e21ce173aefcaf6ca3f19d59bc
79065 <realmerm@yandex.ru>
2025-06-15 16:47:43 +0300

    fix bag

--- src/main/java/com/example/QuoteBotApplication.java
+++ src/main/java/com/example/QuoteBotApplication.java
@@ -1,19 +1,15 @@
 package com.example.quotebot;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.Slf4j; // Убедитесь, что эта строка есть
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
-@Slf4j
+@Slf4j // Эта аннотация для логера
 public class QuoteBotApplication {
 
-    /**
-     * @param args аргументы командной строки, позволяет передавать данные прямо при запуске приложения
-     */
     public static void main(String[] args) {
-        // Эта строка запускает всё приложение Spring Boot.
         SpringApplication.run(QuoteBotApplication.class, args);
-        log.info("Приложение запущено");
+        log.info("Приложение запущено"); // Это ваше сообщение
     }
-
 }
--- src/main/java/com/example/bot/QuoteTelegramBot.java
+++ src/main/java/com/example/bot/QuoteTelegramBot.java
@@ -8,7 +8,6 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import com.example.quotebot.service.QuoteService;
-
 @Component
 @Slf4j
 public class QuoteTelegramBot extends TelegramLongPollingBot {
